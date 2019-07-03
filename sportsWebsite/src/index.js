50f8f7f2739e0b317f905d4af69e84b96a7e9257
Shashank G S <49735860+shashankGS10@users.noreply.github.com>
2019-07-03 23:54:23 +0530

    final changes

--- sportsWebsite/src/index.js
+++ sportsWebsite/src/index.js
@@ -1,4 +1,6 @@
 var dropdown_var = document.getElementById("player_drop_down_container");
+var brk = document.createElement('br');
+
 var slideIndex = 0;
 function showSlides() {
   var i;
@@ -18,118 +20,88 @@ function showSlides() {
 }
 showSlides();
 
-// function handle_dropdown(){
-//   dropdown_var.innerHTML='';
-//   var no_of_participants= document.getElementById("select_drop").value;
-//   for(var i=1;i<=no_of_participants;i++){
-//       var txt = document.createElement('input');
-//       txt.setAttribute('type','text');
-//       txt.setAttribute('class','participants_name');
-//       dropdown_var.appendChild(txt);
-//       var brk = document.createElement('br');
-//       dropdown_var.appendChild(brk);
-
-//   }
-// };
+function handle_dropdown_knockout(){
+  dropdown_var.innerHTML = "";
+  var no_of_participants= document.getElementById("select_drop").value;
+  var counter = 0;
+  console.log(no_of_participants);
+  while(no_of_participants>=1)
+  {
+      console.log(no_of_participants);
+      var row_container = document.createElement("div");
+      row_container.style.display = "inline";
+      row_container.style.width = "20%";
+      row_container.style.position = "absolute";
+      row_container.style.top = 65+(2*counter)+"%";
+      if(no_of_participants===1)
+      {
+        row_container.style.top = 65+(2*(counter-1))+"%";    
+      
+      }
+      row_container.style.left = (20*(counter))+"%";
+      for(var i=0;i<no_of_participants;i++)
+      {
+        var txt = document.createElement('input');
+        txt.setAttribute('type','text');
+        txt.setAttribute('class','participants_name');
+        row_container.appendChild(txt);
+        
+        row_container.appendChild(brk);
+      }
+      dropdown_var.appendChild(row_container);      
+      counter+=1;
+      console.log(counter);
+      if(no_of_participants===1){
+        break;
+      }
+      else
+        no_of_participants=Math.ceil(no_of_participants/2);
+  }
+};
+//----------------Changes under progress..---------------------------//
 function handle_dropdown_league(){
   dropdown_var.innerHTML='';
   var no_of_participants= document.getElementById("select_drop").value;
-  
-  if(no_of_participants%2==0&& no_of_participants<16){
-    for(var j=1;j<=no_of_participants/2;j++){
-      var div1 = document.createElement('div');
-      div1.setAttribute('class','set_a1'+j);
-      dropdown_var.appendChild(div1);
-        for(var i=1;i<=j;i++){
-            var txt = document.createElement('input');
-            txt.setAttribute('type','text');
-            dropdown_var.appendChild(txt);
-            var brk = document.createElement('br');
-            dropdown_var.appendChild(brk);
-        }
-        var brk = document.createElement('br');
-        dropdown_var.appendChild(brk);
-    }
-    for(var j=no_of_participants/2;j>=0;j--){
-      var div1 = document.createElement('div');
-      div1.setAttribute('class','set_a2'+j);
-      dropdown_var.appendChild(div1);
-        for(var i=1;i<=j;i++){
-            var txt = document.createElement('input');
-            txt.setAttribute('type','text');
-            dropdown_var.appendChild(txt);
-            var brk = document.createElement('br');
-            dropdown_var.appendChild(brk);
-        }
-        var brk = document.createElement('br');
-            dropdown_var.appendChild(brk);
-    }
-  }
-  //----------------------------------------------------------------//
-  if(no_of_participants%2==0 && no_of_participants>=16){
-    for(var j=1;j<=no_of_participants/4;j++){
-      
-      var div1 = document.createElement('div');
-      div1.setAttribute('class','set_1'+j);
-      dropdown_var.appendChild(div1);
-        for(var i=1;i<=j;i++){
-            var txt = document.createElement('input');
-            txt.setAttribute('type','text');
-            txt.setAttribute('class','participants_name');
-            dropdown_var.appendChild(txt);
-            var brk = document.createElement('br');
-            dropdown_var.appendChild(brk);
-        }
-        var brk = document.createElement('br');
-            dropdown_var.appendChild(brk);
-    }
-    for(var j=no_of_participants/4;j>=0;j--){
-      var div1 = document.createElement('div');
-      div1.setAttribute('class','set_2'+j);
-      dropdown_var.appendChild(div1);
-        for(var i=1;i<=j;i++){
-            var txt = document.createElement('input');
-            txt.setAttribute('type','text');
-            txt.setAttribute('class','participants_name');
-            dropdown_var.appendChild(txt);
-            var brk = document.createElement('br');
-            dropdown_var.appendChild(brk);
-        }
-        var brk = document.createElement('br');
-            dropdown_var.appendChild(brk);
-    }
-    for(var j=1;j<=no_of_participants/4;j++){
-      var div1 = document.createElement('div');
-      div1.setAttribute('class','set_3'+j);
-      dropdown_var.appendChild(div1);
-      for(var i=1;i<=j;i++){
-          var txt = document.createElement('input');
-          txt.setAttribute('type','text');
-          txt.setAttribute('class','participants_name');
-          dropdown_var.appendChild(txt);
-          var brk = document.createElement('br');
-          dropdown_var.appendChild(brk);
-      }
-      var brk = document.createElement('br');
-          dropdown_var.appendChild(brk);
-  }
-  for(var j=no_of_participants/4;j>=0;j--){
+  var counter = 1;
+  for(var i=1;i<=no_of_participants;i++){
     var div1 = document.createElement('div');
-    div1.setAttribute('class','set_4'+j);
+    div1.setAttribute('class','set_a1');
     dropdown_var.appendChild(div1);
-      for(var i=1;i<=j;i++){
-          var txt = document.createElement('input');
-          txt.setAttribute('type','text');
-          txt.setAttribute('class','participants_name');
-          dropdown_var.appendChild(txt);
-          var brk = document.createElement('br');
-          dropdown_var.appendChild(brk);
-      }
-      var brk = document.createElement('br');
-          dropdown_var.appendChild(brk);
+    var txt = document.createElement('input');
+    txt.setAttribute('type','text');
+    div1.appendChild(txt);
+    div1.appendChild(brk);
   }
+   
+    
+  no_of_participants=no_of_participants<7 ? 2 : 4;
+  while(no_of_participants>=1)
+  {
+    console.log(no_of_participants);
+    var row_container = document.createElement("div");
+    row_container.style.display = "inline";
+    row_container.style.width = "20%";
+    row_container.style.position = "absolute";
+    row_container.style.top = 65+(2*counter)+"%";
+    if(no_of_participants===1)
+    {
+      row_container.style.top = 65+(2*(counter-1))+"%";    
+    }
+    row_container.style.left = (20*(counter))+"%";
+    for(var i=0;i<no_of_participants;i++)
+    {
+      var txt = document.createElement('input');
+      txt.setAttribute('type','text');
+      txt.setAttribute('class','participants_name');
+      row_container.appendChild(txt);
+      row_container.appendChild(brk);
+    }
+    dropdown_var.appendChild(row_container);      
+    counter+=1;
+    console.log(counter);
+    if(no_of_participants===1) break;
+    else
+      no_of_participants=Math.ceil(no_of_participants/2);
   }
-};
+}
 //----------------------------------------------------------------------------//
-
-
